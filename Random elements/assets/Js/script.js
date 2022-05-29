aebf851377b3c37e7a5a7d51c2c512be1c8c86e6
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-05-29 14:05:46 +0430

    Create one rectangle with fixed width, height and color

--- Random elements/assets/Js/script.js
+++ Random elements/assets/Js/script.js
@@ -1,27 +1,8 @@
-let redValue, greenValue, blueValue;
-
-function createBoxes() {
-  let body = document.getElementsByTagName("body")[0];
-  let canvas = document.createElement("canvas");
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
-  let context = canvas.getContext("2d");
-
-  for (let i = 0; i < 100; i++) {
-    redValue = Math.floor(Math.random() * 256);
-    greenValue = Math.floor(Math.random() * 256);
-    blueValue = Math.floor(Math.random() * 256);
-
-    let bgColor = "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
-    context.fillStyle = bgColor;
-
-    context.fillRect(
-      Math.random() * window.innerWidth,
-      Math.random() * window.innerHeight,
-      Math.random() * 60 + 40,
-      Math.random() * 60 + 40
-    );
-  }
-  body.appendChild(canvas);
-}
-window.onload = createBoxes;
+let rectangle = document.createElement("div");
+rectangle.style.width = 400 + "px";
+rectangle.style.height = 200 + "px";
+rectangle.style.backgroundColor = "#f1c40f";
+rectangle.style.position = "absolute";
+rectangle.style.left = "100" + "px";
+rectangle.style.top = "120" + "px";
+document.body.appendChild(rectangle);

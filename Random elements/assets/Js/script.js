d2967fbfcd915eae76608d7e909fe2c84755463d
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-05-29 16:44:33 +0430

    Add a function for moving each box when hover on it to random location

--- Random elements/assets/Js/script.js
+++ Random elements/assets/Js/script.js
@@ -26,3 +26,19 @@ function getRandomColor() {
 
   return bgColor;
 }
+//Move each box when hover on it to random location
+let rectangles = document.getElementsByTagName("div");
+console.log(rectangles);
+//foor...of loop can iterate over the values. Each of div in body
+for (let rectangle of rectangles) {
+  rectangle.addEventListener("mouseover", moveRectangle);
+}
+
+//fuction for moving each random box to a random location
+function moveRectangle(event) {
+  let xPos = Math.floor(Math.random() * innerWidth);
+  let yPos = Math.floor(Math.random() * innerHeight);
+  let element = event.target;
+  element.style.setProperty("left", xPos + "px");
+  element.style.setProperty("top", yPos + "px");
+}

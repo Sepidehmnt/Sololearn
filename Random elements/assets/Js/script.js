5a770b4bc3ebdf4b4c3e1ac38ac17901784cda51
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-05-29 15:23:54 +0430

    Create one rectangle with random position nad size

--- Random elements/assets/Js/script.js
+++ Random elements/assets/Js/script.js
@@ -1,8 +1,12 @@
 let rectangle = document.createElement("div");
-rectangle.style.width = 400 + "px";
-rectangle.style.height = 200 + "px";
 rectangle.style.backgroundColor = "#f1c40f";
 rectangle.style.position = "absolute";
-rectangle.style.left = "100" + "px";
-rectangle.style.top = "120" + "px";
 document.body.appendChild(rectangle);
+
+window.onload = makeRandomBox();
+function makeRandomBox() {
+  rectangle.style.left = Math.floor(Math.random() * 200) + "px";
+  rectangle.style.top = Math.floor(Math.random() * 300) + "px";
+  rectangle.style.width = Math.floor(Math.random() * 200) + "px";
+  rectangle.style.height = Math.floor(Math.random() * 300) + "px";
+}

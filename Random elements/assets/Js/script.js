8edfc5450ee7c612d3523bdf2b59fa20462ec3a0
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-06-03 23:38:51 +0430

    Update script file

--- Random elements/assets/Js/script.js
+++ Random elements/assets/Js/script.js
@@ -1,20 +1,29 @@
-window.onload = makeRandomBox();
-//Random boxes with random location,size and color
-function makeRandomBox() {
+window.onload = getRandomElement();
+
+//random size
+function getRandomElement() {
   let numOfRect = Math.floor(Math.random() * 50) + 10;
   for (let i = 0; i < numOfRect; i++) {
-    let rectangle = document.createElement("div");
-    rectangle.style.position = "absolute";
     //start: random x,y
-    rectangle.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    rectangle.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+    let leftSide = Math.floor(Math.random() * window.innerWidth) + "px";
+    let topSide = Math.floor(Math.random() * window.innerHeight) + "px";
     //random width and height
-    rectangle.style.width = Math.floor(Math.random() * 70) + 30 + "px";
-    rectangle.style.height = Math.floor(Math.random() * 60) + 40 + "px";
-    //random color
-    rectangle.style.backgroundColor = getRandomColor();
-    document.body.appendChild(rectangle);
+    let boxWidth = Math.floor(Math.random() * 70) + 30 + "px";
+    let boxHeight = Math.floor(Math.random() * 60) + 40 + "px";
+    makeRectangle(leftSide, topSide, boxWidth, boxHeight);
   }
+}
+//Random boxes with random location,size and color
+function makeRectangle(leftSide, topSide, boxWidth, boxHeight) {
+  let rectangle = document.createElement("div");
+  rectangle.style.position = "absolute";
+  rectangle.style.left = leftSide;
+  rectangle.style.top = topSide;
+  rectangle.style.width = boxWidth;
+  rectangle.style.height = boxHeight;
+  //random color
+  rectangle.style.backgroundColor = getRandomColor();
+  document.body.appendChild(rectangle);
 }
 
 //function for getting random color
@@ -23,9 +32,9 @@ function getRandomColor() {
   let greenValue = Math.floor(Math.random() * 255);
   let blueValue = Math.floor(Math.random() * 255);
   let bgColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-
   return bgColor;
 }
+
 //Move each box when hover on it to random location
 let rectangles = document.getElementsByTagName("div");
 console.log(rectangles);

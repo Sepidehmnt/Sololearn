0dcf6e1c3eacdf23fc95342dc1a9ed0a3c853d18
Sepidehmnt <sepidehmontazer@hotmail.com>
2022-06-04 15:31:20 +0430

    Add a function that generates random numbers

--- Random elements/assets/Js/script.js
+++ Random elements/assets/Js/script.js
@@ -1,43 +1,41 @@
 window.onload = getRandomElement();
 
-//random size
 function getRandomElement() {
-  let numOfRect = Math.floor(Math.random() * 50) + 10;
-  for (let i = 0; i < numOfRect; i++) {
-    //start: random x,y
-    let leftSide = Math.floor(Math.random() * window.innerWidth) + "px";
-    let topSide = Math.floor(Math.random() * window.innerHeight) + "px";
-    //random width and height
-    let boxWidth = Math.floor(Math.random() * 70) + 30 + "px";
-    let boxHeight = Math.floor(Math.random() * 60) + 40 + "px";
-    makeRectangle(leftSide, topSide, boxWidth, boxHeight);
-  }
+  let numOfRect = generateRand(50, 10);
+  for (let i = 0; i < numOfRect; i++) makeRectangle();
 }
-//Random boxes with random location,size and color
-function makeRectangle(leftSide, topSide, boxWidth, boxHeight) {
+//random size
+function generateRand(multiplier, operand = 0) {
+  return Math.floor(Math.random() * multiplier) + operand;
+}
+function makeRectangle() {
   let rectangle = document.createElement("div");
   rectangle.style.position = "absolute";
-  rectangle.style.left = leftSide;
-  rectangle.style.top = topSide;
-  rectangle.style.width = boxWidth;
-  rectangle.style.height = boxHeight;
-  //random color
+
+  rectangle.style.left = generateRand(window.innerWidth) + "px";
+  rectangle.style.top = generateRand(window.innerHeight) + "px";
+
+  rectangle.style.width = generateRand(70, 30) + "px";
+  rectangle.style.height = generateRand(60, 40) + "px";
+
   rectangle.style.backgroundColor = getRandomColor();
   document.body.appendChild(rectangle);
 }
 
 //function for getting random color
 function getRandomColor() {
-  let redValue = Math.floor(Math.random() * 255);
-  let greenValue = Math.floor(Math.random() * 255);
-  let blueValue = Math.floor(Math.random() * 255);
+  let redValue = generateRand(255);
+  console.log(redValue);
+
+  let greenValue = generateRand(255);
+  let blueValue = generateRand(255);
   let bgColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
   return bgColor;
 }
 
 //Move each box when hover on it to random location
 let rectangles = document.getElementsByTagName("div");
-console.log(rectangles);
+
 //foor...of loop can iterate over the values. Each of div in body
 for (let rectangle of rectangles) {
   rectangle.addEventListener("mouseover", moveRectangle);
@@ -45,8 +43,8 @@ for (let rectangle of rectangles) {
 
 //fuction for moving each random box to a random location
 function moveRectangle(event) {
-  let xPos = Math.floor(Math.random() * innerWidth);
-  let yPos = Math.floor(Math.random() * innerHeight);
+  let xPos = generateRand(innerWidth);
+  let yPos = generateRand(innerHeight);
   let element = event.target;
   element.style.setProperty("left", xPos + "px");
   element.style.setProperty("top", yPos + "px");
